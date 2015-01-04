effdadb13f4302cb6ec8c8f73712dd3adee74dea
Tadas Tamosauskas <tadas@pdfcv.com>
2015-01-04 16:05:50 +0000

    prev/next links to jobs and SEo friendly URLs

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,6 +14,22 @@ class Job < ActiveRecord::Base
     visible_until && visible_until < Time.now
   end
 
+  def full_title
+    "#{title} at #{company_name}"
+  end
+
+  def next_visible
+    @next_visible ||= self.class.visible.order("id ASC").where("id > ?", id).first
+  end
+
+  def prev_visible
+    @prev_visible ||= self.class.visible.order("id DESC").where("id < ?", id).first
+  end
+
+  def to_param
+    "#{id}-remote-#{full_title.parameterize}"
+  end
+
 private
 
   def generate_token
