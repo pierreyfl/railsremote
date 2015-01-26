721b6c398d6836c25f76020262d6ba3c7b4972c5
Tadas Tamosauskas <tadas@pdfcv.com>
2015-01-26 22:18:47 +0000

    Add sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'jobs#index'
   resources :jobs
+  resource :sitemap, only: [:show], defaults: { format: :xml }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
